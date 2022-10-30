e0e47737f6b6fa60304ae3995d4301dcced7bf03
DavidD95~ <daviddaganzofraile@gmail.com~>
2022-10-30 18:47:25 +0100

    Con fondo weeeeee

--- js/Background.js
+++ js/Background.js
@@ -3,7 +3,9 @@ class Background {
   constructor(ctx, canvasSize) {
     this.ctx = ctx;
     this.canvasSize = canvasSize,
-      this.backgroundPos = { x: this.canvasSize.w, y: this.canvasSize.h },
+      this.backgroundPosX = 0
+    this.backgroundPosY = 0
+    this.backgroundSize = { w: this.canvasSize.w, h: this.canvasSize.h },
       this.image = new Image(),
       this.image.src = "../images/background.jpg";
 
@@ -12,7 +14,7 @@ class Background {
 
 
   draw() {
-    this.ctx.drawImage(this.image, this.backgroundPos.w, this.backgroundPos.y);
+    this.ctx.drawImage(this.image, this.backgroundPosX, this.backgroundPosY, this.backgroundSize.w, this.backgroundSize.h);
   }
 }
 
